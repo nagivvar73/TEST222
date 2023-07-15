e48dafb527aa84944ab4d3199a041861322410d4
mahideep <mahideepnagireddi@gmail.com>
2023-07-15 09:17:07 +0530

    qa7

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm123RRRRSSSSSSSSSSSSSSSSSS')
-    print_hi('QQQQQQQQQQQQRRRRRSSSSSSSSSSSSSSSSS')
-    print_hi('RRRRRRRRSSSSSTTTTTTTTTTTTTTTTTTTTTTTTTT')
-    print_hi('PyCharm123444444TTTTTTTTTTTTTTTTT')
-    print_hi('HHHHHHHHLLLLLLTTTTTTTTTTTTTTTTTTTTTTTTTT')
-    print_hi('EEEEEEEEE')
+    print_hi('PyCharm123')
+    print_hi('QQQQQQQQQQQQRRRRR')
+    print_hi('RRRRRRRRSSSSS')
+    print_hi('PyCharm123444444')
+    print_hi('HHHHHHHHLLLLLLT')
+    print_hi('EEEEEEEEEEEFFF')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
